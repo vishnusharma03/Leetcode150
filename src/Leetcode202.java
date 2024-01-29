5bafc324688bf5dbd4f064baa82bbb2f526f8b3c
Vishnu Sharma <vishnusharma9677@gmail.com>
2024-01-29 20:21:42 +0530

    D16P1 Leetcode202

---
+++ src/Leetcode202.java
@@ -0,0 +1,37 @@
+package src;
+
+import javax.naming.TimeLimitExceededException;
+import java.util.Arrays;
+import java.util.HashMap;
+
+public class Leetcode202 {
+    public static void main(String[] args) {
+        int n = 19;
+        System.out.println(isHappy(n));
+    }
+
+    public static boolean isHappy(int n) {
+        HashMap<Integer, Integer> map = new HashMap<>();
+
+        int sOne = square(n);
+        while (sOne != 1) {
+            boolean sTwo = map.containsKey(sOne);
+            if (sTwo){
+                return false;
+            }else {
+                map.put(sOne, 0);
+                sOne = square(sOne);
+            }
+        }
+        return true;
+    }
+    public static int square(int n){
+        int s = 0;
+        while (n > 0) {
+            int rem = n % 10;
+            s += rem * rem;
+            n = n/10;
+        }
+        return s;
+    }
+}

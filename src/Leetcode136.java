992480ec33279529fe5b5ca191528b54de4cef6a
Vishnu Sharma <vishnusharma9677@gmail.com>
2024-02-09 00:24:21 +0530

    D25P1 Leetcode136

---
+++ src/Leetcode136.java
@@ -0,0 +1,16 @@
+package src;
+
+public class Leetcode136 {
+    public static void main(String[] args) {
+        int[] nums = {2,2,1};
+        System.out.println(singleNumber(nums));
+    }
+
+    public static int singleNumber(int[] nums) {
+        int ans = nums[0];
+        for (int i = 1; i < nums.length; i++) {
+            ans = ans ^ nums[i];
+        }
+        return ans;
+    }
+}

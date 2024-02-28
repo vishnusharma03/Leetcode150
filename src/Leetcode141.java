b7602e029a84dde993515b020cc7fefefe229cbf
vishnusharma7 <vishnusharma9677@gmail.com>
2024-02-29 00:41:23 +0530

    D40P1

--- src/Leetcode141.java
+++ src/Leetcode141.java
@@ -4,25 +4,27 @@ import java.util.HashSet;
 
 public class Leetcode141 {
 
-    private class ListNode {
-        int val;
-        ListNode next;
+     // Definition for singly-linked list.
+      class ListNode {
+          int val;
+          ListNode next;
+          ListNode(int x) {
+             val = x;
+             next = null;
+          }
+      }
 
-        ListNode(int x) {
-            val = x;
-            next = null;
-        }
-
-    }
 
     public boolean hasCycle(ListNode head) {
-        HashSet<ListNode> map = new HashSet<>();
-//        ListNode.Node() temp = head.head;
-//        while (temp != null){
-//            map.add(head);
-//            temp = temp.nextOne(temp.head);
-//            if (map.contains(temp)) return true;
-//        }
+        ListNode s = head;
+        ListNode f = head;
+
+        while (f!=null && f.next!=null){
+            s = s.next;
+            f = f.next.next;
+            if (s==f)
+                return true;
+        }
         return false;
     }
 }

093701edb2ae1e4f1c06768aaab2c4d724c38972
vishnusharma7 <vishnusharma9677@gmail.com>
2024-03-03 16:00:26 +0530

    D42P1 Leetcode92

---
+++ src/Leetcode92.java
@@ -0,0 +1,138 @@
+package src;
+
+import java.awt.geom.FlatteningPathIterator;
+
+public class Leetcode92 {
+
+    // Definition for singly-linked list.
+    public class ListNode {
+        int val;
+        ListNode next;
+
+        ListNode() {
+        }
+
+        ListNode(int val) {
+            this.val = val;
+        }
+
+        ListNode(int val, ListNode next) {
+            this.val = val;
+            this.next = next;
+        }
+    }
+
+    // Beats 100%
+    public ListNode reverseBetween(ListNode head, int left, int right) {
+        if (head==null || head.next==null) return head;
+        ListNode prev = null;
+        ListNode temp = head;
+        int i = 1;
+        while (i < left-1){
+            temp = temp.next;
+            i++;
+        }
+
+        ListNode t1 = temp;
+        ListNode t2 = temp.next;
+        if (left!=1)
+            temp = temp.next;
+
+        ListNode lat = temp.next;
+        i = left;
+
+        while (i!=right+1 && temp!=null){
+            temp.next = prev;
+            prev = temp;
+            temp = lat;
+            if (lat!=null){
+                lat = lat.next;
+            }
+            i++;
+        }
+        if (left!=1) {
+            t2.next = temp;
+            t1.next = prev;
+        }else t1.next = temp;
+
+        if (left!=1)
+            return head;
+        else return prev;
+    }
+
+}
+
+    // Iteratino 2
+//    public ListNode reverseBetween(ListNode head, int left, int right) {
+//        if (head==null || head.next==null) return head;
+//        ListNode prev = null;
+//        ListNode temp = head;
+//        int i = 1;
+//        while (i < left-1){
+//            temp = temp.next;
+//            i++;
+//        }
+//
+//        ListNode t1 = temp;
+//        ListNode t2 = temp.next;
+//        if (left!=1)
+//            temp = temp.next;
+//
+//        ListNode lat = temp.next;
+//        i = left;
+//
+//        while (i!=right+1 && temp!=null){
+//            temp.next = prev;
+//            prev = temp;
+//            temp = lat;
+//            if (lat!=null){
+//                lat = lat.next;
+//            }
+//            i++;
+//        }
+//        if (left!=1) {
+//            t2.next = temp;
+//            t1.next = prev;
+//        }else t1.next = temp;
+//
+//        if (left!=1)
+//            return head;
+//        else return prev;
+//    }
+
+
+
+    // Working for left >=2 && right <= length
+//    public ListNode reverseBetween(ListNode head, int left, int right) {
+//        if (head==null || head.next==null) return head;
+//        ListNode prev = null;
+//        ListNode temp = head;
+//        int i = 1;
+//        while (i < left-1){
+//            temp = temp.next;
+//            System.out.println(i);
+//            i++;
+//        }
+//
+//        ListNode t1 = temp;
+//        ListNode t2 = temp.next;
+//        temp = temp.next;
+//        ListNode lat = temp.next;
+//        i = left;
+//
+//        while (i!=right+1 && temp!=null){
+//            temp.next = prev;
+//            prev = temp;
+//            temp = lat;
+//            if (lat!=null){
+//                lat = lat.next;
+//            }
+//            i++;
+//        }
+//
+//        t2.next = temp;
+//        t1.next=prev;
+//
+//        return head;
+//    }
+//}

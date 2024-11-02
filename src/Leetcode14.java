7b00e611cb9c784710110b8b175c37093ee995c5
vishnusharma7 <vishnusharma9677@gmail.com>
2024-11-02 10:53:04 +0530

    Leetcode14

--- src/Leetcode14.java
+++ src/Leetcode14.java
@@ -1,25 +1,23 @@
 package src;
 
-
 public class Leetcode14 {
     public static void main(String[] args) {
-        String[] s1 = {"flower","flow","flight"};
+        String[] s1 = {"ab", "a"}; // {"flower","flow","flight"};
         String ans = longestCommonPrefix(s1);
         System.out.println(ans);
     }
 
     public static String longestCommonPrefix(String[] strs) {
-        String s = "";
-        for (int i = 0; i < strs.length; i++) {
-            String c = strs[i].substring(0,i);
-            for (int j = 0; j < strs.length; j++) {
-                if(c.charAt(i) != strs[j].charAt(i)){
-                    return s;
-                }else{
-                    s = strs[0].substring(0, i);
+        if (strs==null || strs.length==0) return "";
+        int index = 0;
+
+        while(true){
+            for (int i = 0; i < strs.length; i++) {
+                if (index>=strs[i].length() || strs[i].charAt(index)!=strs[0].charAt(index)){
+                    return strs[0].substring(0, index);
                 }
             }
+            index++;
         }
-        return "";
     }
 }
